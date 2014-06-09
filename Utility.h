e28b33b748f6f15f507b22c686ae3abcd4e450c2
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-08 19:11:07 -0600

    Update Utility.h

--- Utility.h
+++ Utility.h
@@ -1,6 +1,21 @@
 //all members and methods should be static
+#ifndef SE_UT
+#define SE_UT
 
-class Utitlity{
-private:
+#include <regex>
+#include <string>
+
+using namespace std;
+
+class Utility
+{
 public:
-}
+	//functions to correlate with user in input
+	static bool IsInt(string s);   //determines if string parses to an int
+	static bool IsNum(string s);   //determines if string represents number
+	static bool IsPosNum(string s); //determines if string is positive number
+	static string Trim(string s); //returns value of string with all leading and trailing white space eliminated
+};
+
+void RunUtilityTests(void);
+#endif
