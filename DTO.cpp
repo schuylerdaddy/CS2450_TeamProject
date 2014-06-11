ae3b1b61c20f7fbec91e06029542d9e26de9bbf4
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-11 17:43:12 -0600

    Update DTO.cpp

--- DTO.cpp
+++ DTO.cpp
@@ -1,5 +1,40 @@
 #include "DTO.h"
 
+
+// Purpose: To initialize needed variables
+// Parameters: None
+// Returns: None
+// Pre-conditions: None
+// Post-conditions: Specific variables initialized
+// -----------------------------------------------------------------
+DueDate::DueDate(int m,int d,int y)
+{
+  if(m>12 || m<1 || d<1 || d>31)
+    throw runtime_error("invalid date");
+  month = m;
+  day = d;
+  year = y;
+}
+
+int DueDate::GetMonth()
+{
+  return month;
+}
+
+int DueDate::GetDay()
+{
+  return day;
+}
+
+int DueDate::GetYear()
+{
+  return year;
+}
+
+DueDate::~DueDate()
+{
+}
+
 //define constructors and function implementations here
 // The constructor
 // Purpose: To initialize needed variables
