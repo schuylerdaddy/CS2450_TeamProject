b2da19081265e6abfffdc7a0fd6b859c9857f9d8
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-11 17:27:28 -0600

    Update DTO.h

--- DTO.h
+++ DTO.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -23,4 +24,23 @@ private:
 	int month;
 	int day;
 	int year;
+};
+
+// This class models a DueDate. It contains key information
+// about a checked-out-item and methods to manage that data
+class Patron
+{
+public:
+	Patron(string,string, bool);
+	vector<int> GetBookList(int);
+        void AssignPatronId();
+	~Patron();
+private:
+    bool idAssigned;
+	bool adult;
+	vector<int> bookList;
+	string patronFirstName;
+	string patronLastName;
+	int patronID; // In case there are 2 people with the same name we can use their library id card number
+	int age;
 };
