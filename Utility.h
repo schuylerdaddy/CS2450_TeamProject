2d024102e6d2ace17ab8e4bf00e15d7ba38b0c9b
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-12 17:17:48 -0600

    Update Utility.h

--- Utility.h
+++ Utility.h
@@ -4,17 +4,25 @@
 
 #include <regex>
 #include <string>
+#include <map>
 
 using namespace std;
 
+enum MediaTypes{AdultBook,ChildBook,DVD,VideoTape};
+    
+
 class Utility
 {
+private:
+    Utility();
 public:
 	//functions to correlate with user in input
 	static bool IsInt(string s);   //determines if string parses to an int
 	static bool IsNum(string s);   //determines if string represents number
 	static bool IsPosNum(string s); //determines if string is positive number
-	static string Trim(string s); //returns value of string with all leading and trailing white space eliminated
+	static string Trim(string s); //returns value of string with all leading and trailing white space
+    static map <MediaTypes,int> DaysByItemType;
+    void LoadDaysByItemType();
 };
 
 void RunUtilityTests(void);
