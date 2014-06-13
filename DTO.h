8dfc8147b9b122cd1395804c0967730907a89856
pixie1 <kjosien1@gmail.com>
2014-06-13 13:03:01 -0600

    Update DTO.h

--- DTO.h
+++ DTO.h
@@ -1,22 +1,20 @@
-#include"Utility.h"
-//include class prototypes/headers here
-
 #pragma once
 #include <string>
 #include <vector>
 
 using namespace std;
 
+
 // This class models a DueDate. It contains key information
 // about a checked-out-item and methods to manage that data
 class DueDate
 {
 public:
-	DueDate(int,int,int);
+	DueDate();
+	DueDate(int, int, int);
 	int GetMonth();
 	int GetDay();
 	int GetYear();
-	~DueDate();
 private:
 	int month;
 	int day;
@@ -28,12 +26,23 @@ private:
 class Patron
 {
 public:
-	Patron(string,string, bool);
+	Patron(string, string, bool);
 	vector<int> GetBookList(int);
-        void AssignPatronId();
+	void AssignPatronId(int);
+	static Patron ReadPatron(istream&);
+	void SavePatron(ostream&);
+	int getID(){ return patronID; };
 	~Patron();
 private:
-    bool idAssigned;
+	struct PatronRec{
+			int id;
+			char lastName[15];
+			char firstName[15];
+			int age;
+			bool adult;
+			int book[6];
+	};
+	bool idAssigned;
 	bool adult;
 	vector<int> bookList;
 	string patronFirstName;
@@ -42,29 +51,38 @@ private:
 	int age;
 };
 
-#include <string>
-
-using namespace std;
 
 // This class models a Book. It contains key information
 // about a book and methods to manage that data
-class Book
+class Media
 {
-	public:
-		Book(string, string);
-		string GetAuthor(); // What should I pass here? bookID? or title?
-		string GetTitle(); // What should I pass here? bookID?
-		bool GetCheckedInStatus(); // What should I pass here? bookID? Title?
-		void CheckIn(); // Should there be a value passed here? bookID? Title?
-		void CheckOut(); // Should there be a value passed here? bookID? Title?
-		void AssignId(int);
-		~Book();
-		// Do we want SetBookID() if we plan on adding books, movies, etc.? 
-		// maybe, just below the iceburg line
-	private:
-		bool idAssigned;
-		string author;
-		string title;
-		int bookID;
-		bool checkedIn;
+public:
+	Media(string, string, int);
+	string GetAuthor();
+	string GetTitle(); // What should I pass here? bookID?
+	bool GetCheckedInStatus(); // What should I pass here? bookID? Title?
+	void CheckIn(); // Should there be a value passed here? bookID? Title?
+	void CheckOut(); // Should there be a value passed here? bookID? Title?
+	void AssignId(int);
+	int getID();
+	void Save(ostream&);
+	static Media read(istream&);
+	~Media();
+
+private:
+	bool idAssigned;
+	string author;
+	string title;
+	int mediaID;
+	bool checkedIn;
+	int type;
+	DueDate due;
+	struct MediaRec{
+		int id;
+		char author[15];
+		char title[30];
+		bool checkIn;
+		int type;
+		DueDate due;
+	};
 };
