7095691078047e76ee6a335a5a114ff49d9f6cc7
pixie1 <kjosien1@gmail.com>
2014-06-13 12:51:47 -0600

    Update DataAccess.h

--- DataAccess.h
+++ DataAccess.h
@@ -1,7 +1,29 @@
-#include DTO.h
+#pragma once
+#include"DTO.h"
+#include <fstream>
+#include <string>
+#include<vector>
 
-//all members and methods should be static
-class DataAccess{
-public:
-private:
-}
+const int BOOK_BLOCKSIZE = 512;
+const int PATRON_BLOCKSIZE = 512;
+class DataAccess
+{
+private: 
+	 std::fstream patronStream;
+	 std::fstream bookStream;
+	 int maxBooks;
+	 int maxPatrons;
+
+public:	
+	DataAccess(){};
+	DataAccess(std::string, std::string);
+	void openBookFile(std::string);
+	void openPatronFile(std::string);
+	void closePatronFile();
+	Media readBook(int);
+	vector<Media> readBook();
+	void saveBook(Media);
+	Patron readPatron(int);
+	vector<Patron> readPatron();
+	void savePatron(Patron);
+};
