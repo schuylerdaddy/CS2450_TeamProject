be465245a76866bd536c20732afbd70c8ac75833
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-15 17:40:05 -0600

    Update Utility.h
    added day object enhancements: get current date as string, Is leap year?, and access to a map that gives amount of days by the number of month

--- Utility.h
+++ Utility.h
@@ -21,8 +21,12 @@ public:
 	static bool IsNum(string s);   //determines if string represents number
 	static bool IsPosNum(string s); //determines if string is positive number
 	static string Trim(string s); //returns value of string with all leading and trailing white space
-    static map <MediaTypes,int> DaysByItemType;
-    void LoadDaysByItemType();
+    	static map <MediaTypes,int> DaysByItemType;
+    	static void LoadDaysByItemType();
+    	static string GetCurrentDate();  //returns current date as "mm-dd-yyyy"
+	static bool IsLeapYear(int year);  //give a year and will return if is leap year or not
+	static map<int, int> GetDayCountbyMonth();  //gives amount of days to month, asssume non leap year
+	static map<int, int> GetDayCountbyMonth(int year);  //determines if year is leap year and returns a map for days by month number
 };
 
 void RunUtilityTests(void);
