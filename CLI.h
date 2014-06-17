078bfb99095506698b39217243d3c86df912967c
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-17 09:57:42 -0600

    Update CLI.h
    Nathan's updates

--- CLI.h
+++ CLI.h
@@ -1,4 +1,28 @@
-class CLI{
+#pragma once
+#include "DataAccess.h"
+#include "Utility.h"
+#include<string>
+using std::string;
+
+class BusinessRules
+{
 private:
+	static DataAccess library;
+	static Date today;
+	Date CalculateDueDate(Media, Date);
+	//Date CalculateToday();
 public:
-}
+	BusinessRules();
+	void CheckOutItem(string patronId, string itemId);
+	void CheckInItem(string patronId, string itemId);
+	void ListAllItems();
+	void ListAllOverdueItems();
+	void ListBooksByPatron(string patronId);
+	void ListAllPatrons();
+	void AdvanceItemDate(string itemId);//????
+	void AddLibraryItem(string author, string title, MediaTypes itemType);
+	Date AdvanceDate();
+	void OpenFiles(string, string);
+	void AddPatron(string, string, bool);
+
+};
