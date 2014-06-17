e789b22d0a36c297e30ea895ee32fe47958b3e8f
schuylerdaddy <gregoryshodgson@gmail.com>
2014-06-17 09:59:01 -0600

    Update CLI.h

--- CLI.h
+++ CLI.h
@@ -1,28 +1,35 @@
 #pragma once
-#include "DataAccess.h"
-#include "Utility.h"
-#include<string>
-using std::string;
+#include "BusinessRules.h"
+#include <vector>
 
-class BusinessRules
+using namespace std;
+
+// This class models a Command Line Interface. It contains key information
+// in order to implement an interactive interface for the user
+class CLI
 {
-private:
-	static DataAccess library;
-	static Date today;
-	Date CalculateDueDate(Media, Date);
-	//Date CalculateToday();
 public:
-	BusinessRules();
-	void CheckOutItem(string patronId, string itemId);
-	void CheckInItem(string patronId, string itemId);
-	void ListAllItems();
-	void ListAllOverdueItems();
-	void ListBooksByPatron(string patronId);
-	void ListAllPatrons();
-	void AdvanceItemDate(string itemId);//????
-	void AddLibraryItem(string author, string title, MediaTypes itemType);
-	Date AdvanceDate();
-	void OpenFiles(string, string);
-	void AddPatron(string, string, bool);
-
+	CLI();
+	~CLI();
+	int GetIntegerInput();
+	char GetCharInput();
+	string GetStringInput();
+	void MenuOptions();
+	void RunUserCommand(char, Date&);
+	void CheckOutItem(string, string);
+	void CheckInItem(string, string);
+	vector<string> ListAllItems();
+	vector<string> ListAllOverdueItems();
+	vector<string> ListBooksByPatron(string);
+	void AdvanceItemDate(string);
+	void AddLibraryItem(string, string, int); // Not sure what to pass here for Mediatypes in utility.h (used int for now)
+private:
+	BusinessRules br; 
+	int integerInput;
+	char charInput;
+	string stringInput;
+	Date date;
+	string patronID; // was int
+	string mediaID; // was int
 };
+
