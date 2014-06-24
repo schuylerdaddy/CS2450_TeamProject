3aece8efc30e8f7034ec3c17917bdca93e13b199
pixie1 <kjosien1@gmail.com>
2014-06-23 21:52:58 -0600

    Update TestDriver.cpp

--- TestDriver.cpp
+++ TestDriver.cpp
@@ -7,7 +7,128 @@ int main()
 	RunUtilityTests();
 	return 0;
 }
+void RunDataAccessTests(){
+	//expected failure
+	DataAccess data;
+	try {
+		data.openBookFile("book");
+	}
+	catch (runtime_error &e)
+	{
+		string temp = e.what();
+		if (temp != "Book list file does not exist")
+			throw runtime_error("failure: opens file that does not exist");
+		else cout << "success\n";
+	}
 
+	try {
+		data.openPatronFile("patron");
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "Patron list file does not exist")
+			throw runtime_error("failure: opens file that does not exist");
+		else cout << "success\n";
+	}
+	try {
+		data.readBook(10);
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "Book does not exist" && temp != "error reading media information")
+			throw runtime_error("failure: read book that does not exist");
+		else cout << "success\n";
+	}
+	Media book("Victor Hugo", "Les Miserables", MediaTypes(0));
+	try{
+		data.addBook(book);
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "overflow, error saving book information" && temp != "error saving book information")
+			throw runtime_error("failure: saving book in non existant file");
+		else
+			cout << "success\n";
+	}
+	DataAccess library;
+	//should work
+	library.openBookFile("bookfile.bin");
+	if (!library.mediaIsOpen())
+		throw runtime_error("File not opened correctly");
+
+	library.openPatronFile("patronfile.bin");
+	if (!library.patronIsOpen())
+		throw runtime_error("File not opened correctly");
+
+	Media book1("Victor Hugo", "Les Miserables", MediaTypes(0));
+	library.addBook(book1);
+	int id = book1.getID();
+	Media testBook = library.readBook(id);
+	if (testBook.GetAuthor() != "Victor Hugo" || testBook.GetTitle() != "Les Miserables")
+		throw runtime_error("error retrieving or saving record");
+
+	Patron patron1("John", "Smith", true);
+	library.addPatron(patron1);
+	int pId = patron1.getID();
+	Patron testPatron = library.readPatron(pId);
+	string displayPatron1 = patron1.displayPatronInfo();
+	string displayTestPatron = testPatron.displayPatronInfo();
+	if (displayPatron1 != displayTestPatron)
+		throw runtime_error("error retrieving or saving patron");
+}
+
+void RunCTOTests(){
+	//invalid date
+	try {
+		Date testDate(0, 0, 0);
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "invalid date")
+			throw runtime_error("failure: invalid date accepted");
+		else
+			cout << "Date Success\n";
+	}
+	try{
+		Date testDate(13, 0, 0);
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "invalid date")
+			throw runtime_error("failure: invalid date accepted");
+		else
+			cout << "Date Success\n";
+	}
+	try{
+		Date testDate(1, 32, 1);
+	}
+	catch (runtime_error &e){
+		string temp = e.what();
+		if (temp != "invalid date")
+			throw runtime_error("failure: invalid date accepted");
+		else
+			cout << "Date Success\n";
+	}
+	
+	//expected success
+	Date date1(1, 1, 2014);
+	date1 += 1;
+	if (date1.GetDay() != 2 || date1.GetMonth() != 1 || date1.GetYear() != 2014)
+		throw runtime_error("Failure: incorrect addition");
+	Date date2(1, 31, 2014);
+	date2 += 2;
+	if (date2.GetDay() != 2 || date2.GetMonth() != 2 || date2.GetYear() != 2014)
+		throw runtime_error("Failure: incorrect addition");
+	Date date3(12, 31, 2014);
+	date3 += 1;
+	if (date3.GetDay() != 1 || date3.GetMonth() != 1 || date3.GetYear() != 2015)
+		throw runtime_error("Failure: incorrect addition");
+	if (date2 > date3)
+		throw runtime_error("failure: incorrect comparison");
+
+
+
+}
 void RunUtilityTests()
 	{
 		//confirm expected failures
